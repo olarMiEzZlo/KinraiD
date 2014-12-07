31654dfb2a08a1fa9a80f6e6bcb95122e40ef51f
Phakphum <s7070088@kmitl.ac.th>
2014-12-08 01:21:51 +0700

    Edit Eating class and add background image, button, input box and back method

--- Kinraid.py
+++ Kinraid.py
@@ -60,18 +60,22 @@ class Main(object):
 
     def call_eating(self):
         """ call Main class to create mainpage """
+        self.root.destroy()
         Eating()
 
     def call_addmenu(self):
         """ call Main class to create mainpage """
+        self.root.destroy()
         Addmenu()
 
     def call_helpus(self):
         """ call Main class to create mainpage """
+        self.root.destroy()
         Helpus()
 
     def call_about(self):
         """ call Main class to create mainpage """
+        self.root.destroy()
         About()
 
 class Eating(object):
@@ -79,10 +83,47 @@ class Eating(object):
     def __init__(self):
         """ main method """
         self.root = Tk()
-        self.root.geometry("605x505+400+100")
+        self.root.geometry("405x505+400+100")
         self.root.title("KinraiD")
 
+        #---Background Image---#
+        bg_eat = PhotoImage(file = "testbg_main.gif")
+        b_eat = Label(self.root, image = bg_eat)
+        b_eat.place(x=0, y=0)
 
+        #---Button---#
+        back_button = PhotoImage(file = "back_but.gif")
+        go_button = PhotoImage(file = "go_but.gif")
+        backbut = Button(self.root, bg = "white", image = back_button,
+                         command = self.back)
+        gobut = Button(self.root, bg = "white", image = go_button)
+        backbut.place(x=280, y=420)
+        gobut.place(x=280, y=170)
+
+        #---Input box---#
+        self.entrymoney = IntVar()
+        Entry(self.root, textvariable = self.entrymoney, bg = "pink"
+              ).place(x=170, y=50)
+        self.name = StringVar()
+        Entry(self.root, textvariable = self.name, bg = "pink"
+              ).place(x=170, y=80)
+
+        #---Label text---#
+        money_text = Label(self.root, text = "Money :", bg = "white"
+                           ).place(x=120, y=50)
+        name_text = Label(self.root, text = "Restaurant Name :", bg = "white"
+                          ).place(x=66, y=80)
+        baht_text = Label(self.root, text = "Baht", bg = "white"
+                          ).place(x=300, y=50)
+        
+
+        self.root.mainloop()
+
+    def back(self):
+        """ back to main page """
+        self.root.destroy()
+        Main()
+        
 class Addmenu(object):
     """ class to input user's money and say what do you eat """
     def __init__(self):
